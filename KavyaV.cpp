71c44ca7579c5b768a84a744836844914b3221d0
KavyaV00 <kavyav5700@gmail.com>
2018-10-30 21:11:45 +0530

    added c++

---
+++ KavyaV.cpp
@@ -0,0 +1,23 @@
+/* 
+Author: KavyaV00
+Language: C++
+Github: Hacktoberfest
+*/
+
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+	clrscr();
+	for(i=1;i<=100;i++)
+		if((i%3==0)&&(i%5==0))
+			cout<< "Spiders\n";
+		else if(i%3==)
+			cout<< "Cats\n";
+		else if(i%5==0)
+			cout<< "Bats\n";
+		else 
+			cout<< i << "\n";
+	getch();
+}
