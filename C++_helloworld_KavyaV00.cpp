ffd719911d3b0c3c0759f12ae62e93cada828e7f
KavyaV00 <kavyav5700@gmail.com>
2018-10-30 21:45:59 +0530

    Hello World in C++

---
+++ C++_helloworld_KavyaV00.cpp
@@ -0,0 +1,14 @@
+// LANGUAGE: C++
+// ENV: Node.cpp
+// AUTHOR: KavyaV00
+// GITHUB: https://github.com/mcao
+
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+	cout<< "Hello, World!";
+	getch();
+}
