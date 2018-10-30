841b9515e9c44916e01ed4cc2fe6aea56689a07e
KavyaV00 <kavyav5700@gmail.com>
2018-10-30 21:28:47 +0530

    Hello World in C++

---
+++ hello_world_KavyaV00.cpp
@@ -0,0 +1,16 @@
+/*
+LANGUAGE: C++
+ENV: virtualenv
+AUTHOR: KavyaV00
+GITHUB:  https://github.com/LordDrragonLive
+*/
+
+#include <iostream.h>
+#include <conio.h>
+
+void main()
+{
+	clrscr();
+	cout<< "Hello, World!";
+	getch();
+}
