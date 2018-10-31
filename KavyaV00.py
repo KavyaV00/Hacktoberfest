fcf5a0b3ec3ad2b1db6d6d0b0d00bf6c33cdb360
KavyaV00 <kavyav5700@gmail.com>
2018-10-31 20:32:18 +0530

    Python

---
+++ KavyaV00.py
@@ -0,0 +1,13 @@
+//AUTHOR: KavyaV00
+//LANGUAGE: Python
+//GITHUB: www.github.com/Cats_Bats_Spiders
+
+for i in range(1,101):
+	if((i%5==0)andi%3==0)):
+		print("Spiders")
+	elif(i%3==0):
+		print("Cats")
+	elif(i%5==0):
+		print("Bats")
+	else:
+		print(i)
